b09fe51c5f3b1a63af93708009ee296e5148d5b8
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-02-16 12:16:48 -0600

    REFACTOR: overrides merging and argument resolving

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
 import faker from 'faker';
 import { FactoryGenerator, IDataObject, IFactoryObject } from './types';
 
-export const resolveArgs = (...args: any[]): IDataObject => args.reduce((resolved, arg) => {
-  if (typeof arg === 'number') {
-    return { ...resolved, length: arg > 0 ? arg : 1 };
-  } else if (typeof arg === 'object') {
-    return { ...resolved, data: arg };
-  } else {
-    return resolved;
-  }
-}, { length: 1, data: {} });
+export const resolveArgs = (...args: any[]): IDataObject =>
+  args.reduce(
+    (resolved, arg) => {
+      if (typeof arg === 'number') {
+        return { ...resolved, length: arg > 0 ? arg : 1 };
+      } else if (typeof arg === 'object') {
+        return { ...resolved, data: arg };
+      } else {
+        return resolved;
+      }
+    },
+    { length: 1, data: {} },
+  );
 
 export const merge = (data: IDataObject, overrides: IDataObject) => {
-  const merged = Object.keys(data).reduce((values, key) => {
-    if (values.wasMerged) {
-      values.data = { ...values.data, [key]: data[key] };
-    } else if (!overrides[key]) {
-      values.data = { ...values.data, [key]: typeof data[key] !== 'object' ? data[key] : merge(data[key], overrides) };
-    } else {
-      values.data = { ...values.data, [key]: overrides[key] };
-      values.wasMerged = true;
-    }
+  const merged = Object.keys(data).reduce(
+    (values, key) => {
+      if (values.wasMerged) {
+        values.data = { ...values.data, [key]: data[key] };
+      } else if (!overrides[key]) {
+        values.data = {
+          ...values.data,
+          [key]: typeof data[key] !== 'object' ? data[key] : merge(data[key], overrides),
+        };
+      } else {
+        values.data = { ...values.data, [key]: overrides[key] };
+        values.wasMerged = true;
+      }
 
-    return values;
-  }, { wasMerged: false, data: {} })
+      return values;
+    },
+    { wasMerged: false, data: {} },
+  );
 
   return merged.data;
 };
