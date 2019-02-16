f608fd51490db54737ef3c6d95bb613e0e111d16
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-02-16 12:14:04 -0600

    TESTS: refactored tests

--- .huskyrc.js
+++
@@ -1,7 +0,0 @@
-const tasks = (...arr) => arr.join(' && ');
-
-module.exports = {
-  hooks: {
-    'pre-commit': tasks('yarn test', 'yarn format', 'yarn lint', 'yarn build'),
-  },
-};
--- doczrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-  dest: 'docs/dist',
-  title: 'Node Factory',
-  description: 'Easy way to create random data for your tests',
-  typescript: true,
-};
