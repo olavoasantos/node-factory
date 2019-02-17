c8a97dbf1e79b881d2c777287a71f0b59e4eb30f
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-02-16 18:36:01 -0600

    DOCS: added github links to docs

--- doczrc.js
+++ doczrc.js
@@ -6,6 +6,12 @@ export default {
     'Main',
     { name: 'API', menu: ['factory', 'create', 'only', 'make', 'seed', 'types'] },
     { name: 'Examples'},
+    'Update from v0.1.2',
+    { name: 'GitHub', menu: [
+      { name: 'Repo', href: 'https://github.com/olavoasantos/node-factory/' },
+      { name: 'Issues', href: 'https://github.com/olavoasantos/node-factory/issues' },
+      { name: 'Pull requests', href: 'https://github.com/olavoasantos/node-factory/pulls' },
+    ] },
   ],
   themeConfig: {
     styles: {
