5edce6c4dda38662c3079d0de63df19be751da0e
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-02-17 08:36:27 -0600

    TEST: added test to check seed method using make

--- src/__tests__/seed.test.ts
+++ src/__tests__/seed.test.ts
@@ -15,4 +15,12 @@ describe('seed tests', () => {
     expect(data1).toMatchObject(data2);
     expect(data1).not.toMatchObject(data3);
   });
+
+  it('should return the same values when using make', () => {
+    const data1 = Factory.seed(123).make(2);
+    const data2 = Factory.seed(123).make(2);
+
+    expect(data1[0]).toMatchObject(data2[0]);
+    expect(data1[1]).toMatchObject(data2[1]);
+  });
 });
